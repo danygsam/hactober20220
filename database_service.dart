92a202a09cccab7dc5fd1d950de43c461890fccc
Dany G Sam <70808552+danygsam@users.noreply.github.com>
2020-10-26 07:39:13 +0530

    recieving data from firebse

---
+++ database_service.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  Future<void> addQuizData(Map quizData, String quizId) async {
+    await FirebaseFirestore.instance
+        .collection('categories')
+        .doc(quizId)
+        .set(quizData)
+        .catchError((e) {
+      print(e.toString());
+    });
+  }
+
+  getCategories({limit}) async {
+    await FirebaseFirestore.instance.collection('categories').orderBy('name').limit(limit).snapshots();
+  }
+}
